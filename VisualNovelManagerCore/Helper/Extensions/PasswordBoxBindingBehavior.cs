af5c4d78b9a464b46d1c0a8804fcc5fec7d87edb
Micah <legendofzeldafan3@gmail.com>
2019-04-27 11:08:35 -0700

    Added  more helper methods

---
+++ VisualNovelManagerCore/Helper/Extensions/PasswordBoxBindingBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VisualNovelManagerCore.Helper.Extensions
+{
+    public class PasswordBoxBindingBehavior
+    {
+        private void OnPasswordBoxValueChanged(object sender, RoutedEventArgs e)
+        {
+            try{}
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
