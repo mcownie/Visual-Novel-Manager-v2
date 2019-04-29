adbcad715d6b0f517ce3e97df96afe1d924bf034
Micah <legendofzeldafan3@gmail.com>
2019-04-28 21:15:15 -0700

    Added two more helper methods

---
+++ VisualNovelManagerCore/Helper/IsJpnText.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace VisualNovelManagerCore.Helper
+{
+    internal class IsJpnText
+    {
+        private bool IsJapaneseText(string text)
+        {
+            Regex regex = new Regex(@"/[\u3000-\u303F]|[\u3040-\u309F]|[\u30A0-\u30FF]|[\uFF00-\uFFEF]|[\u4E00-\u9FAF]|[\u2605-\u2606]|[\u2190-\u2195]|\u203B/g");
+            return regex.IsMatch(text);
+        }
+    }
+}
---
+++ VisualNovelManagerCore/Helper/ValidateExe.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualNovelManagerCore.Helper
+{
+    internal class ValidateExe
+    {
+        internal static bool ValidateExecutable(string FileName)
+        {
+            try
+            {
+                if (!File.Exists(FileName)) return false;
+                byte[] twoBytes = new byte[2];
+                using (FileStream fileStream = File.Open(FileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    fileStream.Read(twoBytes, 0, 2);
+                }
+                switch (Encoding.UTF8.GetString(twoBytes))
+                {
+                    case "MZ":
+                        return true;
+                    case "ZM":
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+                throw;
+            }
+        }
+    }
+}
