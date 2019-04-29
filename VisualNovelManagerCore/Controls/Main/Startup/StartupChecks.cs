8501740d6b644c62face5464d8b57c7b08de4a1d
Micah <legendofzeldafan3@gmail.com>
2019-04-28 18:52:33 -0700

    Added basic startup checks

---
+++ VisualNovelManagerCore/Controls/Main/Startup/StartupChecks.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VisualNovelManagerCore.Services.Main.Startup;
+
+namespace VisualNovelManagerCore.Controls.Main.Startup
+{
+    public class StartupChecks:IStartupChecks
+    {
+        public void RunStartupChecks()
+        {
+            CreateFolders();
+        }
+
+        private void CreateFolders()
+        {
+            Directory.CreateDirectory(Globals.DirectoryPath + @"\Data\config");
+            Directory.CreateDirectory(Globals.DirectoryPath + @"\Data\Database");
+            Directory.CreateDirectory(Globals.DirectoryPath + @"\Data\vndb\images\characters");
+            Directory.CreateDirectory(Globals.DirectoryPath + @"\Data\vndb\images\cover");
+            Directory.CreateDirectory(Globals.DirectoryPath + @"\Data\vndb\images\screenshots");
+            Directory.CreateDirectory(Globals.DirectoryPath + @"\Data\libs\");
+            Directory.CreateDirectory(Globals.DirectoryPath + @"\Data\res\icons");
+            Directory.CreateDirectory(Globals.DirectoryPath + @"\Data\res\icons\country_flags");
+        }
+    }
+}
