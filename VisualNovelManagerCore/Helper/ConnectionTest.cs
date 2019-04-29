2c8a8fb2ed59cd0a7e019126cb13b2cd4be0cc22
Micah <legendofzeldafan3@gmail.com>
2019-04-28 20:35:08 -0700

    Added ConnectionTest and ValidateXML

---
+++ VisualNovelManagerCore/Helper/ConnectionTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualNovelManagerCore.Helper
+{
+    internal class ConnectionTest
+    {
+        internal static bool VndbTcpSocketTest()
+        {
+            try
+            {
+                TcpClient client = new TcpClient("api.vndb.org", 19535);
+                client.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ VisualNovelManagerCore/Helper/ValidateXml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace VisualNovelManagerCore.Helper
+{
+    internal class ValidateXml
+    {
+        internal static bool IsValidXml(string input)
+        {
+            XmlReaderSettings settings = new XmlReaderSettings
+            {
+                CheckCharacters = true,
+                ConformanceLevel = ConformanceLevel.Document,
+                DtdProcessing = DtdProcessing.Ignore,
+                IgnoreComments = true,
+                IgnoreProcessingInstructions = true,
+                IgnoreWhitespace = true,
+                ValidationFlags = XmlSchemaValidationFlags.None,
+                ValidationType = ValidationType.None,
+            };
+
+            using (XmlReader xmlReader = XmlReader.Create(input, settings))
+            {
+                try
+                {
+                    while (xmlReader.Read()) {} // Reader intentionally left empty
+                    return true;
+                }
+                catch (XmlException)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
