21c60126ccfe0c905ce1b7608d419f825e76d89b
Micah <legendofzeldafan3@gmail.com>
2019-05-18 13:00:19 -0700

    Added CreateIconHelper

---
+++ VisualNovelManagerCore/Helper/CreateIconHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace VisualNovelManagerCore.Helper
+{
+    public static class CreateIconHelper
+    {
+        private static BitmapSource CreateIcon(string path)
+        {
+            try
+            {
+                if (path == null)
+                {
+                    return BitmapSource.Create(1, 1, 96, 96, PixelFormats.Bgra32, null, new byte[] { 0, 0, 0, 0 }, 4);
+                }
+                Icon sysicon = System.Drawing.Icon.ExtractAssociatedIcon(path);
+                if (sysicon == null)
+                    return BitmapSource.Create(1, 1, 96, 96, PixelFormats.Bgra32, null, new byte[] { 0, 0, 0, 0 }, 4);
+                BitmapSource bmpSrc = System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(
+                    sysicon.Handle, System.Windows.Int32Rect.Empty,
+                    System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+                sysicon.Dispose();
+                return bmpSrc;
+            }
+            catch (Exception ex)
+            {                
+                throw;
+            }
+
+        }
+    }
+}
