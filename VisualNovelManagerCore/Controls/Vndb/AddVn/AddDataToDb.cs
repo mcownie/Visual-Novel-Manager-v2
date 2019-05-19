847d579b4cb6ddda33a94ba8be09b86b72d5ac38
Micah <legendofzeldafan3@gmail.com>
2019-05-19 13:02:03 -0700

    Added GetVnData

---
+++ VisualNovelManagerCore/Controls/Vndb/AddVn/AddDataToDb.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using VisualNovelManagerCore.Database.Model.VNDB.VnInfo;
+using VndbSharp.Models;
+using VndbSharp.Models.Common;
+using VndbSharp.Models.VisualNovel;
+using LiteDB;
+
+namespace VisualNovelManagerCore.Controls.Vndb.AddVn
+{
+    class AddDataToDb
+    {
+        private void AddToDb()
+        {
+
+        }
+
+        private async Task AddVnInfo(VndbResponse<VisualNovel> visualNovels)
+        {
+            if (visualNovels.FirstOrDefault() != null)
+            {
+                VisualNovel visualNovel = visualNovels.FirstOrDefault();
+
+                VnInfo dbInfo = new VnInfo()
+                {
+                    VnId = visualNovel.Id,
+                    Title = visualNovel.Name,
+                    Original = visualNovel.OriginalName,
+                    Released = visualNovel.Released?.ToString() ?? null,
+                    Languages = ConvertToCsv(visualNovel.Languages),
+                    OriginalLanguages = ConvertToCsv(visualNovel.OriginalLanguages),
+                    Platforms = ConvertToCsv(visualNovel.Platforms),
+                    Aliases = ConvertToCsv(visualNovel.Aliases),
+                    Length = visualNovel.Length?.ToString(),
+                    Description = visualNovel.Description,
+                    ImageLink = visualNovel.Image,
+                    ImageNsfw = visualNovel.IsImageNsfw,
+                    Popularity = visualNovel.Popularity,
+                    Rating = visualNovel.Rating
+                };
+
+                
+            }
+        }
+
+        private string ConvertBirthday(SimpleDate birthday)
+        {
+            string formatted = string.Empty;
+            if (birthday == null) return formatted;
+            if (birthday.Month == null) return birthday.Month == null ? birthday.Day.ToString() : string.Empty;
+            string month = System.Globalization.DateTimeFormatInfo.InvariantInfo.GetMonthName(Convert.ToInt32(birthday.Month));
+            formatted = $"{month} {birthday.Day}";
+            return formatted;
+        }
+
+        string ConvertToCsv(ReadOnlyCollection<string> input)
+        {
+            return input != null ? string.Join(",", input) : null;
+        }
+    }
+}
---
+++ VisualNovelManagerCore/Controls/Vndb/AddVn/GetVnData.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VisualNovelManagerCore.Helper.Vndb;
+using VndbSharp.Models;
+using VndbSharp.Models.Character;
+using VndbSharp.Models.Release;
+using VndbSharp.Models.VisualNovel;
+using VndbSharp.Filters;
+using VndbSharp;
+
+
+namespace VisualNovelManagerCore.Controls.Vndb.AddVn
+{
+    class GetVnData
+    {
+        private async Task GetData(uint _vnid)
+        {
+            try
+            {
+                using (VndbSharp.Vndb client = new VndbSharp.Vndb(true))
+                {
+                    bool hasMore = true;
+                    RequestOptions ro = new RequestOptions { Count = 25 };
+                    int pageCount = 1;
+                    int characterCount = 0;
+                    int releasesCount = 0;
+
+                    VndbResponse<VisualNovel> visualNovels = await client.GetVisualNovelAsync(VndbFilters.Id.Equals(_vnid), VndbFlags.FullVisualNovel);
+                    if (visualNovels == null)
+                    {
+                        HandleError.HandleErrors(client.GetLastError(), 0);
+                        return;
+                    }
+
+                    List<Character> characterList = new List<Character>();
+                    while (hasMore)
+                    {
+                        ro.Page = pageCount;
+                        VndbResponse<Character> characters = await client.GetCharacterAsync(VndbFilters.VisualNovel.Equals(_vnid), VndbFlags.FullCharacter, ro);
+                        if (characters != null)
+                        {
+                            hasMore = characters.HasMore;
+                            characterList.AddRange(characters.Items);
+                            characterCount = characterCount + characters.Count;
+                            pageCount++;
+                        }
+                        if (characters != null) continue;
+                        HandleError.HandleErrors(client.GetLastError(), 0);
+                        return;
+                    }
+
+                    hasMore = true;
+                    pageCount = 1;
+
+                    List<Release> releaseList = new List<Release>();
+                    while (hasMore)
+                    {
+                        ro.Page = pageCount;
+                        VndbResponse<Release> releases = await client.GetReleaseAsync(VndbFilters.VisualNovel.Equals(_vnid), VndbFlags.FullRelease, ro);
+                        if (releases == null)
+                        {
+                            HandleError.HandleErrors(client.GetLastError(), 0);
+                            break;
+                        }
+                        hasMore = releases.HasMore;
+                        releaseList.AddRange(releases.Items);
+                        releasesCount = releasesCount + releases.Count;
+                        pageCount++;
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
