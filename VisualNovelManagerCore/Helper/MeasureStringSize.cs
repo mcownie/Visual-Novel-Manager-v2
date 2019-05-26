d809bd0ff3012eff9e40018e4c72aa8e9d7a354f
Micah <legendofzeldafan3@gmail.com>
2019-05-25 21:25:37 -0700

    Set new MeasureStringSize

--- VisualNovelManagerCore/Helper/MeasureStringSize.cs
+++ VisualNovelManagerCore/Helper/MeasureStringSize.cs
@@ -1,4 +1,7 @@
-﻿using System.Globalization;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Windows;
 using System.Windows.Media;
 
@@ -6,10 +9,11 @@ namespace VisualNovelManagerCore.Helper
 {
     public class MeasureStringSize
     {
-        public static double GetMaxStringWidth(string text)
+        public static double SetMaxStringWidth(IEnumerable<string> collection)
         {
+            string longestString = collection.OrderByDescending(s => s.Length).First();
             FormattedText formattedText = new FormattedText(
-                text, CultureInfo.InvariantCulture, FlowDirection.LeftToRight,
+                longestString, CultureInfo.InvariantCulture, FlowDirection.LeftToRight,
                 new Typeface(new System.Windows.Media.FontFamily("Segoe UI"), FontStyles.Normal, FontWeights.Bold, FontStretches.Normal),
                 13, Brushes.Black);
             //add 10 for some extra padding
