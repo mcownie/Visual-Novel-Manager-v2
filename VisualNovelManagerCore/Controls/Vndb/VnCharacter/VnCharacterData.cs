d5005775e5409688db9f351c184cb35a6423f8d1
Micah <legendofzeldafan3@gmail.com>
2019-05-27 13:19:36 -0700

    Added VnCharacterData

---
+++ VisualNovelManagerCore/Controls/Vndb/VnCharacter/VnCharacterData.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace VisualNovelManagerCore.Controls.Vndb.VnCharacter
+{
+    class VnCharacterData
+    {
+        private async Task DownloadCharacters()
+        {
+            try
+            {
+                var vnCharacter = Globals.LiteDbInstance.GetCollection<Database.Model.VNDB.VnCharacter.VnCharacter>("vncharacter");
+                List<string> characterList = vnCharacter.Find(x => x.VnId == Globals.VnId).Select(i => i.ImageLink).ToList();
+                if (characterList.Count > 0)
+                {
+                    using (WebClient client = new WebClient())
+                    {
+                        foreach (string character in characterList)
+                        {
+                            if (!Directory.Exists($@"{Globals.DirectoryPath}\Data\vndb\images\characters\{Globals.VnId}"))
+                            {
+                                Directory.CreateDirectory($@"{Globals.DirectoryPath}\Data\vndb\images\characters\{Globals.VnId}");
+                            }
+                            string path = $@"{Globals.DirectoryPath}\Data\vndb\images\characters\{Globals.VnId}\{Path.GetFileName(character)}";
+
+                            if (!File.Exists(path) && !string.IsNullOrEmpty(character))
+                            {
+                                await client.DownloadFileTaskAsync(new Uri(character), path);
+                            }
+                        }
+                        client.Dispose();
+                    }
+                }
+            }
+            catch (WebException webEx)
+            {
+                Console.WriteLine(webEx);
+                throw;
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex);
+                throw;
+            }
+        }
+    }
+}
--- VisualNovelManagerCore/Globals.cs
+++ VisualNovelManagerCore/Globals.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Reflection;
+using LiteDB;
 
 namespace VisualNovelManagerCore
 {
@@ -14,6 +15,7 @@ namespace VisualNovelManagerCore
         internal static uint VnId;
         internal static bool NsfwEnabled = false;
         internal static SpoilerLevel MaxSpoiler = SpoilerLevel.None;
+        internal static LiteDatabase LiteDbInstance = new LiteDatabase(@"MyData.db");
     }
 
     internal enum SpoilerLevel
